d68e475bc72ccce9816780c3ed4a42e565672eee
Akhila Vuppuluri <akhilav@Akhilas-MacBook-Air.local>
2024-03-04 22:15:12 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL = 'mongodb+srv://akhilavuppuluri:Rajeswari%40123@booksmern.4xq76nw.mongodb.net/book-collection?retryWrites=true&w=majority&appName=booksMERN'
---
+++ index.js
@@ -0,0 +1,19 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+const app = express();
+app.get('/',(request,response)=>{
+    console.log(request)
+    return(response.status(234).send('Welcome to mern stack tutorial'))
+});
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port : ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(error);
+});
